4c740801551787333c587df2a8b66de610c06bf6
Anneka Goss <annekagoss@gmail.com>
2016-08-10 20:42:11 -0400

    array pulse

--- js/gwave.js
+++ js/gwave.js
@@ -11,6 +11,7 @@ var hemisphereLight, shadowLight, mesh, geometry, HEIGHT, WIDTH;
 var mousePos = { x: 0, y: 0 };
 var dataRendered = false;
 var data;
+var nodeArray = [];
 
 function createScene() {
 	// DOM setup
@@ -85,7 +86,14 @@ function onWindowResize() {
 function loop() {
 	camera.lookAt(scene.position);
 	controls.update();
-	if (dataRendered) { node.updateNode(); }
+
+	if (dataRendered) {
+		for (n = 0; n < nodeArray.length; n++) {
+			var phaseOff = Math.floor(nodeArray[n].distance);
+			nodeArray[n].updateNode(phaseOff);
+		}
+	}
+
 	render();
 	requestAnimationFrame(loop);
 }
@@ -116,7 +124,7 @@ function renderData(d) {
 
 var Node = function() {
 	this.mesh = new THREE.Object3D();
-	var geom = new THREE.SphereGeometry(0.25,10,10);
+	var geom = new THREE.SphereGeometry(0.125,10,10);
 	var mat = new THREE.MeshPhongMaterial ({
 		wireframe: true,
 		color:Colors.white
@@ -126,28 +134,60 @@ var Node = function() {
 	n.castShadow = true;
 	n.receiveShadow = true;
 	this.mesh.add(n);
+
 	var counter = 1000;
-	this.updateNode = function() {
-		if (counter === data.length-1001) { counter = 1000; }
-		this.mesh.position.x = data[counter].x;
-		this.mesh.position.y = data[counter].y;
+	this.updateNode = function(phaseOffset) {
+		if (phaseOffset+counter >= data.length-1000) { counter = 1000; }
+		// this.mesh.position.x = data[counter+phaseOffset].x;
+		this.mesh.position.y = this.initialHeight + this.initialHeight * 0.5*data[phaseOffset+counter].y;
+		this.mesh.position.x = this.initialWidth + this.initialWidth * 0.5*data[phaseOffset+counter].y;
+		this.mesh.position.z = this.initialDepth + this.initialDepth * 0.5*data[phaseOffset+counter].y;
 		counter ++;
 	}
 }
 
-function createNode(){
+function createNodeArray(){
+	arrWidth = 10;
+	arrHeight = 10;
+	arrDepth = 10;
+	for (i=arrWidth*-0.5; i<arrWidth*0.5; i++) {
+		for (j=arrHeight*-0.5; j<arrHeight*0.5; j++) {
+			for (k=arrDepth*-0.5; k<arrDepth*0.5; k++) {
+				createNode(i, j, k);
+			}
+		}
+	}
+	console.log(nodeArray[0]);
+}
+function getDist(x, y, z) {
+	var dist = Math.sqrt((x*x) + (y*y) + (z*z));
+	var nodeDist = dist ? dist : 0;
+	return nodeDist;
+}
+
+function createNode(xVal, yVal, zVal) {
 	this.mesh = new THREE.Object3D();
 	var node = new Node();
+	node.mesh.position.x = xVal;
+	node.mesh.position.y = yVal;
+	node.mesh.position.z = zVal;
+	node.initialWidth = xVal;
+	node.initialHeight = yVal;
+	node.initialDepth = zVal;
+	node.distance = getDist(xVal, yVal, zVal);
+
 	this.mesh.add(node.mesh);
 	this.node = node;
 	scene.add(this.mesh);
+	nodeArray.push(this.node);
 }
 
 function init() {
 	createScene();
 	createLights();
 	loadData();
-	createNode();
+	// createNode();
+	createNodeArray();
 	loop();
 }
 
