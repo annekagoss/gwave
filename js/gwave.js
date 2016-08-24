d8756557ef0a48b6e486f8f62d627b41149a19b8
Anneka Goss <annekagoss@gmail.com>
2016-08-23 23:02:45 -0400

    cubemesh created

--- js/gwave.js
+++ js/gwave.js
@@ -21,8 +21,23 @@ var spread = 10;
 var nodeSize = 1;
 var nodeRes = 2;
 var delay = 0;
-var falloff = 1/spread;
-var friction = 0.125;
+
+// var friction = 0.125;
+var friction = .25;
+
+var cubeWidth = 1000,
+    cubeHeight = 1000,
+    cubeDepth = 1000,
+    cubeRes = 10,
+    cubeSpread = 100;
+
+var falloff = .5;
+
+var cubeArray = [],
+    cubeVertices = [],
+    vertexNodes = [];
+
+var lerpDuration = 50;
 
 function createScene() {
 	// DOM setup
@@ -66,7 +81,7 @@ function createScene() {
 
 	// Scene setup
 	scene = new THREE.Scene();
-	scene.fog = new THREE.Fog(Colors.blue, -400, 800);
+	// scene.fog = new THREE.Fog(Colors.blue, -400, 800);
 
 	// Renderer setup
 	renderer = new THREE.WebGLRenderer({
@@ -87,7 +102,7 @@ function createLights() {
 	// the first parameter is the sky color, the second parameter is the ground color,
 	// the third parameter is the intensity of the light
 	hemisphereLight = new THREE.HemisphereLight(Colors.pink,Colors.blue, 1);
-	hemisphereLight.position.set(0, -25, 0);
+	hemisphereLight.position.set(0, 0, 0);
 	scene.add(hemisphereLight);
 }
 
@@ -98,21 +113,48 @@ function onWindowResize() {
 	controls.handleResize();
 }
 
+var frame = 0;
+
+function countFrames() {
+  if (frame === delay) {
+    frame = 0;
+  }
+  // console.log(frame/delay);
+  frame ++
+  requestAnimationFrame(countFrames);
+}
+
 function loop() {
 	camera.lookAt(scene.position);
 	controls.update();
 
-	if (dataRendered) {
-		for (n = 0; n < nodeArray.length; n++) {
-			var phaseOff = Math.floor(nodeArray[n].distance*falloff);
-			nodeArray[n].updateNode(phaseOff);
-		}
+	// if (dataRendered) {
+	// 	for (n = 0; n < nodeArray.length; n++) {
+	// 		var phaseOff = Math.floor(nodeArray[n].distance*falloff);
+	// 		nodeArray[n].updateNode(phaseOff);
+	// 	}
+	// }
+
+  if (dataRendered) {
+    // if (frame === delay) {
+  		for (n = 0; n < vertexNodes.length-1; n++) {
+  			var phaseOff = Math.floor(vertexNodes[n].distance*falloff/cubeSpread);
+  			vertexNodes[n].updateVertexNode(phaseOff);
+  		}
+    // }
 	}
 
 	render();
 	setTimeout(function() {
 		requestAnimationFrame(loop);
 	}, delay);
+}
+
+function lerpPosition(posA, posB, duration, frame) {
+  // console.log(frame/duration);
+  var t = frame/duration;
+  var newPos = posA + t * (posB - posA);
+  return newPos;
 }
 
 function render() {
@@ -157,9 +199,6 @@ var Node = function() {
 
 	this.updateNode = function(phaseOffset) {
 		if (phaseOffset+counter >= data.length-1000) { counter = 1000; }
-		// console.log("phase offset: " + phaseOffset);
-		// console.log("counter: " + counter);
-		// this.mesh.position.x = data[co;unter+phaseOffset].x;
 		this.mesh.position.y = this.initialHeight + this.initialHeight * friction * data[phaseOffset+counter].y;
 		this.mesh.position.x = this.initialWidth + this.initialWidth * friction * data[phaseOffset+counter].y;
 		this.mesh.position.z = this.initialDepth + this.initialDepth * friction * data[phaseOffset+counter].y;
@@ -167,6 +206,72 @@ var Node = function() {
 		this.previousOffset = phaseOffset;
 		counter ++;
 	}
+}
+
+var VertexNode = function(vertex,parent,index) {
+  this.initialX = vertex.x;
+  this.initialY = vertex.y;
+  this.initialZ = vertex.z;
+  this.distance = getDist(vertex.x, vertex.y, vertex.z);
+  this.parentCube = parent.children[0].geometry;
+  this.indexInParent = index;
+
+  var counter = 1000;
+
+	this.updateVertexNode = function(phaseOffset) {
+		if (phaseOffset+counter >= data.length-1000) { counter = 1000; }
+
+		this.parentCube.vertices[this.indexInParent].x = this.initialX + this.initialX * friction * data[phaseOffset+counter].y;
+    this.parentCube.vertices[this.indexInParent].y = this.initialY + this.initialY * friction * data[phaseOffset+counter].y;
+    this.parentCube.vertices[this.indexInParent].z = this.initialZ + this.initialZ * friction * data[phaseOffset+counter].y;
+
+		this.distance = getDist(this.parentCube.vertices[this.indexInParent].x, this.parentCube.vertices[this.indexInParent].y, this.parentCube.vertices[this.indexInParent].z);
+
+		this.previousOffset = phaseOffset;
+		counter ++;
+    this.parentCube.verticesNeedUpdate = true;
+
+	}
+
+}
+
+
+
+function createCubeMesh() {
+  for (i=0;i<cubeWidth;i+=cubeSpread) {
+    var resolution = i < cubeRes ? i : cubeRes;
+    createCube(i,resolution);
+  }
+  setTimeout(function() {
+    cubeArray.forEach(function(cube) {
+      var vertices = cube.children[0].geometry.vertices;
+      // var vertices = cube.vertices;
+      vertices.forEach(function(v) {
+        var vertexNode = new VertexNode(v,cube,vertices.indexOf(v));
+        vertexNodes.push(vertexNode);
+      })
+    });
+  },0);
+  setTimeout(function() {
+    console.log(vertexNodes[0]);
+  },0);
+
+}
+
+function createCube(size, res){
+  this.mesh = new THREE.Object3D();
+	var geom = new THREE.CubeGeometry(size,size,size,res,res,res);
+	var mat = new THREE.MeshPhongMaterial ({
+		wireframe: true,
+		color:Colors.white
+	});
+
+	var n = new THREE.Mesh(geom, mat);
+	// n.castShadow = true;
+	// n.receiveShadow = true;
+	this.mesh.add(n);
+  scene.add(this.mesh);
+  cubeArray.push(this.mesh);
 }
 
 function createNodeArray(){
@@ -178,6 +283,7 @@ function createNodeArray(){
 		}
 	}
 }
+
 function getDist(x, y, z) {
 	var dist = Math.sqrt((x*x) + (y*y) + (z*z));
 	var nodeDist = dist ? dist : 0;
@@ -195,7 +301,6 @@ function createNode(xVal, yVal, zVal, spread) {
 	node.initialDepth = zVal*spread;
 	node.distance = getDist(xVal*spread, yVal*spread, zVal*spread);
 
-
 	this.mesh.add(node.mesh);
 	this.node = node;
 	scene.add(this.mesh);
@@ -206,9 +311,14 @@ function init() {
 	createScene();
 	createLights();
 	loadData();
-	// createNode();
-	createNodeArray();
-	loop();
+	// createNodeArray();
+  // createCube();
+  createCubeMesh();
+  setTimeout(function(){
+    loop();
+    countFrames();
+  },0);
+
 }
 
 init();
