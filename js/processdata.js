ab20c73052be6321ab5b7be67f2120ad5277a7c8
annekagoss <annekagoss@gmail.com>
2016-11-04 01:42:13 -0400

    combined datasets

--- js/processdata.js
+++ js/processdata.js
@@ -1,11 +1,27 @@
 var datasets = [], blackHoleDatasets = [];
 
+var combinedData = [];
+
 var timeStretch = 100; //slow down data so effects can be seen
 var startTime = -5;
 var endTime = 2;
 
-function loadData() {
+function combineData(waveData) {
+	waveData = jQuery.grep(waveData, function(d, i) {
+		return i > waveData.length - blackHoleDatasets[0].data.length+1;
+	});
+	waveData.forEach(function(w){
+		combinedData.push({
+			waveSecs:parseFloat((w.x/timeStretch).toFixed(5)),
+			holeSecs:parseFloat((blackHoleDatasets[0].data[waveData.indexOf(w)].seconds/timeStretch).toFixed(5)),
+			waveVal: w.y,
+			holeDist: blackHoleDatasets[0].data[waveData.indexOf(w)].distance,
+			holeVel: blackHoleDatasets[1].data[waveData.indexOf(w)].velocity
+		});
+	});
+}
 
+function loadData() {
 	function loadH1Data() {
 		$.ajax({
         type: "GET",
@@ -129,12 +145,20 @@ function processData(text, setName) {
         data.push({'x':x,'y':y});
     }
 
+	if (setName === "h1") {
+		var waveData = jQuery.grep(data, function(d, i) {
+			return d.x < 0;
+		});
+		combineData(waveData);
+	}
+
 	data = jQuery.grep(data, function(d, i) {
 		return d.x > startTime && d.x < endTime;
 	});
 
 	var title = setName === "h1" ? "LIGO Hanford Observatory, Mon Sep 14 09:16:37 GMT 2015, 16384 Hz" : "Numerical Relativity Template";
 	datasets.push({name:setName,data:data,title:title});
+
 }
 
 function retrieveDataset(name) {
